ee809b372be6ce89e75e70244b25bee8ea860cc1
adamBacso <47544237+adamBacso@users.noreply.github.com>
2024-02-21 11:18:53 +0100

    title

---
+++ main.py
@@ -0,0 +1,20 @@
+from googletrans import Translator, constants
+import math
+import os
+
+def title_text():
+    os.system('cls')
+    title = "VOCAB BUILDER"
+    version = "0.1.0"
+    boxWidth =70
+    print("+" + ("-"*(boxWidth-2)) + "+")
+    center(title,boxWidth,"|")
+    center(version,boxWidth,"|")
+    print("+" + ("-"*(boxWidth-2)) + "+")
+    print("\n\n")
+
+
+def center(text, width, border = " "):
+    print(border + " "*(math.floor((width-2-len(text))/2)) + text + " "*(math.ceil((width-2-len(text))/2))  + border)
+
+title_text()
