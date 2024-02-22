a08f952e29b46386efa7bbf4d4a7cc9379f6f34b
olivermtoth <oliverm.toth@protonmail.ch>
2024-02-22 14:18:57 +0100

    Creat Word and Deck

---
+++ words.py
@@ -0,0 +1,47 @@
+import json
+
+LANGUAGES = ["nl", "fr", "hu", 'zn_CH']
+
+
+class Deck():
+    
+    def __init__(self, dir="test.json"):
+        self._words = []
+        raw = {}
+        with open(dir, 'r') as f:
+            raw = json.load(f)
+        for x in raw.keys():
+            w = Word(x, raw[x][LANGUAGES[0]], raw[x][LANGUAGES[1]], raw[x][LANGUAGES[2]])
+            self._words.append(w)
+
+    def add_word_man(self, en, nl, fr, hu, zn_CH):
+        ...
+
+        
+
+
+class Word():
+    
+    def __init__(self, en, nl, fr, hu, zn_CH):
+        self._en = en
+        self._nl = nl
+        self._fr = fr
+        self._hu = hu
+        self._zn_CH = zn_CH
+
+    def get_en(self):
+        return self._en
+    
+    def get_nl(self):
+        return self._nl
+    
+    def get_fr(self):
+        return self._fr
+    
+    def get_hu(self):
+        return self._hu
+    
+    def get_zn_CH(self):
+        return self._hu
+    
+d = Deck()
